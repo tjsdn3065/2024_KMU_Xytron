16346f3e689ffccf98f92f08ff9ab0265e437fe1
tjsdn3065 <tjsdn3065@gmail.com>
2024-11-28 14:17:28 +0900

    ar미션 코드

--- ar_follow.py
+++ ar_follow.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import rospy, math
+import rospy, math, time
 from xycar_msgs.msg import xycar_motor
 from yolov5_ROS.msg import Yolo_Objects, Objects
 from ar_track_alvar_msgs.msg import AlvarMarkers
-#from tf.transformations import euler_from_quaternion
+
+
+# from tf.transformations import euler_from_quaternion
 
 class ARDriver:
     def __init__(self):
@@ -23,16 +25,21 @@ class ARDriver:
 
         self.yolo_msg = None
         self.angle = 0
-        self.speed = 5
-        self.variable = 0.50
+        self.speed = 7
+        self.variable = 0.4  # ar마커가 특정 거리 이내에 들어왔을 때 스쳐지나가기 위한 변수
+        self.dis = 2.0  # ar 마커 특정 거리 변수
+        self.add_x_pos = -1.0  # ar 마커가 특정 거리 이내에 들어오기 전에 ar마커 위치로 가기 위한 변수
+        self.add_angle = 150  # 조향각도 곱 변수
+
+        self.cur_id = None
+        self.prev_id = None
+
         self.green_light_detected = False
-        self.dis=1.5
-        self.cur_id=None
-        self.prev_id=None
+
         self.pass_first_ar = False
         self.pass_second_ar = False
         self.pass_third_ar = False
-        self.add_x_pos=0.1
+        self.pass_fourth_ar = False
 
     def yolo_callback(self, msg):
         self.yolo_msg = msg
@@ -57,7 +64,7 @@ class ARDriver:
 
         try:
             for id, dx, dz in zip(self.ar_msg["ID"], self.ar_msg["DX"], self.ar_msg["DZ"]):
-                #distance = math.sqrt(dx ** 2 + dz ** 2)
+                # distance = math.sqrt(dx ** 2 + dz ** 2)
                 if dz < min_dz:
                     min_dz = dz
                     closest_id = id
@@ -79,93 +86,99 @@ class ARDriver:
         for bbox in self.yolo_msg.yolo_objects:
             if bbox.Class == 1:
                 return True  # 녹색 신호등 발견
-            
+
         return False
 
-    
     def drive(self, angle, speed):
         self.motor_msg.angle = int(angle)
         self.motor_msg.speed = int(speed)
         self.motor.publish(self.motor_msg)
 
     def run(self):
-        rate = rospy.Rate(10) # 10hz
+        rate = rospy.Rate(10)  # 10hz
         while not rospy.is_shutdown():
             try:
-                self.cur_id, z_pos, x_pos = self.check_AR()
-                #print(f"pitch: {pitch}")
+                self.cur_id, z_pos, x_pos = self.check_AR()  # 가장 가까운 ar마커 정보 취득
+                # print(f"pitch: {pitch}")
 
-                if self.green_light_detected == False and self.check_green_traffic_light_detected():
-                    #print("its_me")
+                if self.green_light_detected == False and self.check_green_traffic_light_detected():  # 신호등이 초록색이면 -> 변수 값 변경
+                    # print("its_me")
                     self.green_light_detected = True
-                    self.variable = -0.49
-                    self.speed = 5
-                    self.dis=1.0
+                    self.variable = -0.50
+                    self.dis = 1.0
+                    self.add_x_pos = -0.1
+                    self.add_angle = 200
 
-                if self.cur_id == 99:
+                if self.cur_id == 99 and self.pass_third_ar and self.pass_fourth_ar == False:  # ar마커를 찾지 못했고 세번째 ar마커를 통과했고 네번째 ar 마커를 통과하지 않았을 때
+                    self.angle = -20
+
+                elif self.cur_id == 99 and self.pass_second_ar and self.pass_third_ar == False:  # ar마커를 찾지 못했고 두번째 ar마커를 통과했고 세번째 ar 마커를 통과하지 않았을 때
+                    self.angle = 20
+                elif self.cur_id == 99 and self.pass_first_ar and self.pass_second_ar == False and self.prev_id != None:  # ar마커를 찾지 못했고 첫번째 ar마커를 통과했고 두번째 ar 마커를 통과하지 않았을 때
+                    self.angle = 15
+
+                elif self.cur_id == 99:  # ar마커를 찾지 못했을 때
                     self.angle = 0
-                    #self.speed = 5
-                else:
-                    #print(f"ID={ar_ID} Z_pos={z_pos} X_pos={x_pos}")
-                    #distance = math.sqrt(z_pos**2 + x_pos**2)
-                    #print(f"distance: {distance}")
-                    #print(f"z_pos: {z_pos}")
-                    #print(f"self.prev_id: {self.prev_id} self.cur_id: {self.cur_id}")
 
-                    if self.pass_second_ar and self.pass_third_ar == False:
-                        if self.prev_id != None and self.prev_id != self.cur_id:
+                else:  # ar 마커를 찾았다면
+
+                    if self.pass_third_ar and self.pass_fourth_ar == False:  # 3~4 사이
+                        if self.prev_id != None and self.prev_id != self.cur_id:  # 네번째 ar을 통과했다면
+                            self.pass_fourth_ar = True
+                            self.prev_id = None
+                            self.dis = 1.0
+                        elif z_pos < 1.3:
+                            self.prev_id = self.cur_id
+
+                    elif self.pass_second_ar and self.pass_third_ar == False:  # 2~3 사이
+                        if self.prev_id != None and self.prev_id != self.cur_id:  # 세번째 ar을 통과했다면
                             self.pass_third_ar = True
                             self.prev_id = None
-                        elif z_pos < 1.1:
+                            self.dis = 1.35
+                        elif z_pos < 1.3:
                             self.prev_id = self.cur_id
-                    elif self.pass_first_ar and self.pass_second_ar == False:
-                        if self.prev_id != None and self.prev_id != self.cur_id:
+
+                    elif self.pass_first_ar and self.pass_second_ar == False:  # 1~2 사이
+                        if self.prev_id != None and self.prev_id != self.cur_id:  # 두번째 ar을 통과했다면
                             self.pass_second_ar = True
                             self.prev_id = None
-                        elif z_pos < 1.5:
+                            self.dis = 0.95
+                        elif z_pos < 1.3:
                             self.prev_id = self.cur_id
-                    elif self.green_light_detected and self.pass_first_ar == False:
-                        if self.prev_id != None and self.prev_id != self.cur_id:
+
+                    elif self.green_light_detected and self.pass_first_ar == False:  # 0~1 사이
+                        if self.prev_id != None and self.prev_id != self.cur_id:  # 첫번째 ar을 통과했다면
                             self.pass_first_ar = True
                             self.prev_id = None
-                        elif z_pos < 1.5:
+                            self.dis = 1.4
+                        elif 1.3 < z_pos < 2.0:
                             self.prev_id = self.cur_id
 
-                    if self.pass_third_ar:
-                        print("pass_third_ar")
-                        self.dis=1.0
-                        self.add_x_pos = 0.1
-                    elif self.pass_second_ar:
-                        print("pass_second_ar")
-                        self.dis=0.8
-                        self.add_x_pos = 0.2
-                    elif self.pass_first_ar:
-                        print("pass_first_ar")
-                        self.dis = 1.4
-                        self.add_x_pos = 0.1
-                    else:
-                        print("not_pass_first_ar")
-                    
-                    # if self.green_light_detected and pitch > 40:
-                    #     self.dis = 0.8
+                    if z_pos > self.dis:  # ar마커가 특정거리 이상일 때
 
-                    # print(f"self_dis: {self.dis}")
-                    # print(f"self.add_x_pos: {self.add_x_pos}")
+                        x_pos = x_pos + self.add_x_pos
+                        self.angle = x_pos * self.add_angle
 
-                    if z_pos > self.dis:
-                        x_pos = x_pos - self.add_x_pos
-                        self.angle = x_pos * 200
-                        #self.speed = 5
-                    else:
+                        if self.pass_third_ar and self.pass_fourth_ar == False:  # 3~4 사이
+                            if z_pos >= 1.35:
+                                self.angle = -20  # ar마커를 찾았고 세번째 ar마커를 통과했고 네번째 ar 마커를 통과하지 않았을 때
+                        elif self.pass_second_ar and self.pass_third_ar == False:  # 2~3 사이
+                            if z_pos >= 1.35:
+                                self.angle = 20  # ar마커를 찾았고 두번째 ar마커를 통과했고 세번째 ar 마커를 통과하지 않았을 때
+                            elif 0.95 <= z_pos < 1.35:
+                                self.angle = 50
+
+
+                    else:  # 특정거리 이내일 때 -> 스쳐지나가기
                         x_pos = x_pos + self.variable
                         self.angle = x_pos * 200
-                        #self.speed = 5
 
                 self.drive(self.angle, self.speed)
             except Exception as e:
                 rospy.logerr(f"Error in main loop: {str(e)}")
 
             rate.sleep()
+
 
 if __name__ == '__main__':
     try:
